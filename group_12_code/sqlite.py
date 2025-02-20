65815cbfbf7b9badd7512c72846a028bac5a9c04
Jasper Heijne <J.Heijne-1@student.tudelft.nl>
2025-02-20 13:11:20 +0100

    Added comments

--- group_12_code/sqlite.py
+++ group_12_code/sqlite.py
@@ -1,9 +1,12 @@
 import sqlite3
 import pandas as pd
 
-db_path = 'olist.sqlite\olist.sqlite'
+# Put database file: 'olist.sqlite' in same folder as this file. 
+# Download database file: https://www.kaggle.com/datasets/terencicp/e-commerce-dataset-by-olist-as-an-sqlite-database?resource=download
+db_path = 'group_12_code\olist.sqlite'
 db_connection = sqlite3.connect(db_path)
 
+# Generate requests:
 def view_table(table, limit):
     query = f"""
         SELECT *
@@ -11,4 +14,6 @@ def view_table(table, limit):
         LIMIT {limit}
     """
     return pd.read_sql_query(query, db_connection)
+
+# Print results of sql request:
 print(view_table('products', 5))
